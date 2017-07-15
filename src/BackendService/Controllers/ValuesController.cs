0a6385f529afb2f7c6bc0834045e56db9cfa88bf
Steve Gordon <sgordon@hotmail.co.uk>
2017-07-15 15:41:54 +0100

    Initial

---
+++ src/BackendService/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1-frombackend", "value2-frombackend" };
+        }
+    }
+}
