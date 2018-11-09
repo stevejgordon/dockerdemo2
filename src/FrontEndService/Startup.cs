26c5f99eb8b0cbe489409b456c383af65ee35b59
Steve Gordon <sgordon@hotmail.co.uk>
2018-11-09 15:46:13 +0000

    Updating some missed files!

--- src/FrontEndService/Startup.cs
+++ src/FrontEndService/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using FrontEndService.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,7 +21,7 @@ namespace FrontEndService
         {
             services.Configure<EndpointsConfig>(Configuration.GetSection("Endpoints"));
 
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
